99f33498297877186eee4cb383f71213a92d676e
EnriquePolanco <51246027+EnriquePolanco@users.noreply.github.com>
2021-02-19 20:07:05 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './assets/css/App.css';
+
+
+//Componentes
+import MainPage from './HT-Web/MainPage.js';
+import Layout from './layout/layout.js' ;
+import Builder from './HT-Web/Builder.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <MainPage/>
+
+    </div>
+  );
+}
+
+export default App;
