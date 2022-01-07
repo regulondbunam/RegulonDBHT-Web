60aec3f0eb59476a9e913cf3d7cd730c5cce193c
galarcon-ccg <galarcon@ccg.unam.mx>
2022-01-06 22:19:54 -0600

    add features table dataset

--- src/apps/ht/HT-DataSet/home/table.jsx
+++ src/apps/ht/HT-DataSet/home/table.jsx
@@ -1,6 +1,6 @@
-import React, {useMemo} from 'react'
+import React, { useMemo } from 'react'
 import styled from 'styled-components'
-import { useTable, useBlockLayout } from 'react-table'
+import { useTable, useBlockLayout, useGlobalFilter, useAsyncDebounce, useResizeColumns } from 'react-table'
 import { FixedSizeList } from 'react-window'
 import scrollbarWidth from './scrollbarWidth'
 
@@ -26,7 +26,23 @@ const Styles = styled.div`
       padding: 0.5rem;
       border-bottom: 1px solid black;
       border-right: 1px solid black;
+      .resizer {
+        display: inline-block;
+        background: blue;
+        width: 10px;
+        height: 100%;
+        position: absolute;
+        right: 0;
+        top: 0;
+        transform: translateX(50%);
+        z-index: 1;
+        ${'' /* prevents from scrolling while dragging on touch devices */}
+        touch-action:none;
 
+        &.isResizing {
+          background: red;
+        }
+      }
       :last-child {
         border-right: 1px solid black;
       }
@@ -35,82 +51,102 @@ const Styles = styled.div`
 `
 
 function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
-  const defaultColumn = React.useMemo(
-    () => ({
-      width: 150,
-    }),
-    []
-  ) 
-  const scrollBarSize = React.useMemo(() => scrollbarWidth(), [])
-  
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    totalColumnsWidth,
-    prepareRow,
-  } = useTable(
-    {
-      columns,
-      data,
-      defaultColumn,
-    },
-    useBlockLayout
-  )
+    // Use the state and functions returned from useTable to build your UI
+    const defaultColumn = React.useMemo(
+        () => ({
+            minWidth: 100,
+            width: 150,
+            maxWidth: 400,
+        }),
+        []
+    )
+    const scrollBarSize = React.useMemo(() => scrollbarWidth(), [])
 
-  const RenderRow = React.useCallback(
-    ({ index, style }) => {
-      const row = rows[index]
-      prepareRow(row)
-      return (
-        <div
-          {...row.getRowProps({
-            style,
-          })}
-          className="tr"
-        >
-          {row.cells.map(cell => {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        totalColumnsWidth,
+        state,
+        preGlobalFilteredRows,
+        prepareRow,
+        setGlobalFilter
+    } = useTable(
+        {
+            columns,
+            data,
+            defaultColumn,
+        },
+        useBlockLayout,
+        useGlobalFilter,
+        useResizeColumns
+    )
+
+    const RenderRow = React.useCallback(
+        ({ index, style }) => {
+            const row = rows[index]
+            prepareRow(row)
             return (
-              <div {...cell.getCellProps()} className="td">
-                {cell.render('Cell')}
-              </div>
+                <div
+                    {...row.getRowProps({
+                        style,
+                    })}
+                    className="tr"
+                >
+                    {row.cells.map(cell => {
+                        return (
+                            <div {...cell.getCellProps()} className="td">
+                                {cell.render('Cell')}
+                            </div>
+                        )
+                    })}
+                </div>
             )
-          })}
+        },
+        [prepareRow, rows]
+    )
+
+    // Render the UI for your table
+    return (
+        <div {...getTableProps()} className="table">
+            <div className="th" >
+                <GlobalFilter
+                    preGlobalFilteredRows={preGlobalFilteredRows}
+                    globalFilter={state.globalFilter}
+                    setGlobalFilter={setGlobalFilter}
+                />
+            </div>
+            <div>
+                {headerGroups.map(headerGroup => (
+                    <div {...headerGroup.getHeaderGroupProps()} className="tr">
+                        {headerGroup.headers.map(column => (
+                            <div {...column.getHeaderProps()} className="th">
+                                {column.render('Header')}
+                                <div
+                                    {...column.getResizerProps()}
+                                    className={`resizer ${column.isResizing ? 'isResizing' : ''
+                                        }`}
+                                />
+                            </div>
+
+                        ))}
+                    </div>
+                ))}
+            </div>
+
+            <div {...getTableBodyProps()}>
+                <FixedSizeList
+                    height={500}
+                    itemCount={rows.length}
+                    itemSize={40}
+                    width={totalColumnsWidth + scrollBarSize}
+                >
+                    {RenderRow}
+                </FixedSizeList>
+            </div>
         </div>
-      )
-    },
-    [prepareRow, rows]
-  )
-
-  // Render the UI for your table
-  return (
-    <div {...getTableProps()} className="table">
-      <div>
-        {headerGroups.map(headerGroup => (
-          <div {...headerGroup.getHeaderGroupProps()} className="tr">
-            {headerGroup.headers.map(column => (
-              <div {...column.getHeaderProps()} className="th">
-                {column.render('Header')}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-
-      <div {...getTableBodyProps()}>
-        <FixedSizeList
-          height={500}
-          itemCount={rows.length}
-          itemSize={35}
-          width={totalColumnsWidth+scrollBarSize}
-        >
-          {RenderRow}
-        </FixedSizeList>
-      </div>
-    </div>
-  )
+    )
 }
 
 export function DatasetTable({ datasets, datasetType }) {
@@ -151,7 +187,8 @@ export function DatasetTable({ datasets, datasetType }) {
                     },
                     {
                         Header: "Title",
-                        accessor: "_title"
+                        accessor: "_title",
+                        filter: 'fuzzyText',
                     }
                 ]
                 data = datasets.map(dataset => {
@@ -165,10 +202,39 @@ export function DatasetTable({ datasets, datasetType }) {
         return { columns: columns, data: data }
     }, [datasets, datasetType])
 
-  return (
-    <Styles>
-      <Table columns={tableFormat.columns} data={tableFormat.data} />
-    </Styles>
-  )
+    return (
+        <Styles>
+            <Table columns={tableFormat.columns} data={tableFormat.data} />
+        </Styles>
+    )
 }
 
+function GlobalFilter({
+    preGlobalFilteredRows,
+    globalFilter,
+    setGlobalFilter,
+}) {
+    const count = preGlobalFilteredRows.length
+    const [value, setValue] = React.useState(globalFilter)
+    const onChange = useAsyncDebounce(value => {
+        setGlobalFilter(value || undefined)
+    }, 200)
+
+    return (
+        <span>
+            Search:{' '}
+            <input
+                value={value || ""}
+                onChange={e => {
+                    setValue(e.target.value);
+                    onChange(e.target.value);
+                }}
+                placeholder={`${count} records...`}
+                style={{
+                    fontSize: '1.1rem',
+                    border: '0',
+                }}
+            />
+        </span>
+    )
+}
