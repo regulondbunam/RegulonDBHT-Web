2dea13d5cfa60380e478eaa2dbc1a6388eb0578f
galarcon-ccg <galarcon@ccg.unam.mx>
2022-06-02 21:43:34 -0500

    update version

--- src/apps/ht/mainPage/Main.jsx
+++ src/apps/ht/mainPage/Main.jsx
@@ -49,7 +49,7 @@ class Main extends React.Component {
                 }
                 <br />
                 <br />
-                <p style={{ color: "#FFFFFF" }} >0.9.6</p>
+                <p style={{ color: "#FFFFFF" }} >0.9.7</p>
             </article>
         );
     }
